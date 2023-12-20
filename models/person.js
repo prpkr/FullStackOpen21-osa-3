c8f588dea567a9736501d4e36e63f16f7d310d9a
prpkr <kaihyvarinen@gmail.com>
2023-12-20 22:22:30 +0200

    3.22

--- models/person.js
+++ models/person.js
@@ -1,34 +1,48 @@
-require('dotenv').config();
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 
 mongoose.set('strictQuery', false)
 
 
-const password = process.env.PASSWORD;
-const url = process.env.DB_URL.replace('<password>', password);
+const password = process.env.PASSWORD
+const url = process.env.DB_URL.replace('<password>', password)
 
 
 mongoose.connect(url)
   .then(result => {
-    console.log('connected to MongoDB');
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    //Validaatio tehtävä 3.19 ja 3.20
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d{6,7}$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  }
 
-});
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
-});
+})
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
